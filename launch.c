bd9d3429e54a457cb41deca8314ea55190887e35
Chap Carr <78283294+ChapCarr@users.noreply.github.com>
2024-03-11 00:46:17 -0500

    cleaned up comments

--- launch.c
+++ launch.c
@@ -8,20 +8,17 @@ void launch(struct Server *server)
 {
     char *hello = "HTTP/1.1 200 OK\nDate: Sunday, 10 March, 2024\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
     char buffer[30000];
-    // int addrlen = sizeof(server->address);
-    // int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&addrlen);
+   
 
     while (1)
     {
-        // char buffer[30000];
+        
         printf(" **** Waiting For Connection **** \n");
+
         int addrlen = sizeof(server->address);
         int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&addrlen);
         read(new_socket, buffer, 30000);
-
         printf("%s\n", buffer);
-        // char *hello = "HTTP/1.1 200 OK\nDate: Sunday, 10 March, 2024\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-
         write(new_socket, hello, strlen(hello));
         close(new_socket);
     }
--- webserver.h
+++ webserver.h
@@ -11,7 +11,7 @@ typedef struct Server
     int domain;
     int protocol;
     int service;
-    in_addr_t interface; // u_long did not work
+    in_addr_t interface; 
     int port;
     int backlog;
 
